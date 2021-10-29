30360b15ae89e526114e75f64782bd896082fd2f
Malo457 <malostamourbilodeau@gmail.com>
2021-10-29 13:52:49 -0400

    Mal: tests interview passent

--- src/Interview.java
+++ src/Interview.java
@@ -2,8 +2,22 @@ public class Interview {
 
     //TODO: completer
     public static TreeNode solution (TreeNode t1, TreeNode t2) {
-        return null;
+        if (t1 == null && t2 == null) return null;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+
+        TreeNode result = new TreeNode();
+
+        result.val = Math.min(t1.val, t2.val);
+
+        if (t1.left == null && t2.left != null) result.left = t2.left;
+        else if (t1.left != null && t2.left == null) result.left = t1.left;
+        else if (t1.left != null) result.left = solution(t1.left, t2.left);
+
+        if (t1.right == null && t2.right != null) result.right = t2.right;
+        else if (t1.right != null && t2.right == null) result.right = t1.right;
+        else if (t1.right != null) result.right = solution(t1.right, t2.right);
+
+        return result;
     }
 }
-
-
