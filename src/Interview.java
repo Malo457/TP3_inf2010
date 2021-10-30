4cdd3c2622d3c8fbbd2c9ec046c3666b397720ef
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-10-30 16:32:01 -0400

    M: fini

--- src/Interview.java
+++ src/Interview.java
@@ -2,21 +2,13 @@ public class Interview {
 
     public static TreeNode solution (TreeNode t1, TreeNode t2) {
         if (t1 == null && t2 == null) return null;
-        if (t1 == null) return t2;
-        if (t2 == null) return t1;
+        else if (t1 == null) return t2;
+        else if (t2 == null) return t1;
 
         TreeNode result = new TreeNode();
-
         result.val = Math.min(t1.val, t2.val);
-
-        if (t1.left == null && t2.left != null) result.left = t2.left;
-        else if (t1.left != null && t2.left == null) result.left = t1.left;
-        else if (t1.left != null) result.left = solution(t1.left, t2.left);
-
-        if (t1.right == null && t2.right != null) result.right = t2.right;
-        else if (t1.right != null && t2.right == null) result.right = t1.right;
-        else if (t1.right != null) result.right = solution(t1.right, t2.right);
-
+        result.left = solution(t1.left, t2.left);
+        result.right = solution(t1.right, t2.right);
         return result;
     }
 }
